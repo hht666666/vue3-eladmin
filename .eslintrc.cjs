6d49a769b12d841e8716638c4ce48eb48039800f
hht <kui35qvu@163.com>
2024-04-22 19:11:57 +0800

    feat: 配置自动引入

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  "env": {
-    "browser": true,
-    "amd": true,
-    "node": true
-},
+  env: {
+    browser: true,
+    amd: true,
+    node: true
+  },
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
